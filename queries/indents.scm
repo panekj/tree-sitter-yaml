b9babecbe0c742c3a1645a25d785759192d2fa63
panekj <me@panekj.dev>
2022-07-07 17:23:21 +0200

    add queries

---
+++ queries/indents.scm
@@ -0,0 +1,5 @@
+; LICENCE: Mozilla Public License 2.0
+; SPDX: MPL-2.0
+; SOURCE: https://github.com/helix-editor/helix/blob/58758fee610a3808dfaeafddd1b4b4242a7e42cd/runtime/queries/yaml/highlights.scm
+
+(block_mapping_pair) @indent
---
+++ queries/injections.scm
@@ -0,0 +1,6 @@
+; LICENCE: Mozilla Public License 2.0
+; SPDX: MPL-2.0
+; SOURCE: https://github.com/helix-editor/helix/blob/58758fee610a3808dfaeafddd1b4b4242a7e42cd/runtime/queries/yaml/highlights.scm
+
+((comment) @injection.content
+ (#set! injection.language "comment"))
